0ee0322fb299401952700fd2e0d18b3f9870352c
kkukileon305 <kkukileon305@gmail.com>
2023-10-06 16:59:09 +0900

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,9 +44,9 @@ const Page = () => {
           <p className='text-center text-white'>
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don't look even slightly
+            humour, or randomised words which don&apos;t look even slightly
             believable. If you are going to use a passage of Lorem Ipsum, you
-            need to be sure there isn't anything embarrassing hidden in the
+            need to be sure there isn&apos;t anything embarrassing hidden in the
             middle of text. All the Lorem Ipsum generators on the Internet tend
             to repeat predefined chunks as necessary.
           </p>
